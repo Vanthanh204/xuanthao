eeeaf25bac8849d59d36659b2cc3cdbfeba757b4
Vanthanh204 <vanthanh20042021@gmail.com>
2026-04-17 21:39:08 +0700

    fix login admin 5

--- config/flasher.php
+++ config/flasher.php
@@ -1,9 +1,19 @@
 <?php
 
 return [
+    'default' => 'toastr',
+    'main_script' => 'https://cdn.jsdelivr.net/npm/@flasher/flasher@1.3.2/dist/flasher.min.js',
+    'styles' => [],
     'auto_render' => false,
+    'auto_translate' => true,
     'flash_bag' => [
         'enabled' => true,
+        'types' => [
+            'success' => ['success'],
+            'error' => ['error', 'danger'],
+            'warning' => ['warning', 'alarm'],
+            'info' => ['info', 'notice', 'alert'],
+        ],
     ],
     'filter_criteria' => [],
 ];
